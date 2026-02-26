5c72c1a75162e9752c0c2d0754aba9b488a0c081
Kartikeytech123 <kartikeys2007@gmail.com>
2026-02-27 01:04:59 +0530

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 
-const nextConfig: NextConfig = {
-  output: "export",
+  // This fixes assets, links, navigation, client-side routing
+  basePath: '/my-profile-',
+  assetPrefix: '/my-profile-/',
+
+  // Optional but recommended for cleaner URLs
+  // If you ever rename the repo, update these too
 };
 
 export default nextConfig;
